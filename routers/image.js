cf321953264ba26b364d0e1c221947cf6b0aaae0
Itsapon <robintmier@gmail.com>
2021-10-28 11:28:01 +0200

    set up get requests in images and users. currently empty because DB is empty

--- routers/image.js
+++ routers/image.js
@@ -1,11 +1,21 @@
 // import stuff
 const { Router } = require("express");
-const User = require("../models").user;
+const Image = require("../models").image;
 
 //define routes
 const router = new Router();
-
+//basepage
 router.get("/", (req, res) => res.send("Welcome to imagepage!"));
+//get requests
+router.get("/all", async (req, res) => {
+  try {
+    const allImages = await Image.findAll({ raw: true });
+    console.log("requesting all images");
+    res.send(allImages);
+  } catch (e) {
+    console.log(`Oopsie daisy, this aint working. Here's the message: ${e}`);
+  }
+});
 
 //export
 module.exports = router;
--- routers/user.js
+++ routers/user.js
@@ -1,11 +1,40 @@
 // import stuff
 const { Router, response } = require("express");
-const Image = require("../models").image;
+const User = require("../models").user;
 
 //define routes
 const router = new Router();
-
+//basepage
 router.get("/", (req, res) => res.send("Welcome to userpage!"));
+//get requests
+router.get("/all", async (req, res) => {
+  try {
+    const allUsers = await User.findAll({ raw: true });
+    console.log("requesting all users");
+    res.send(allUsers);
+  } catch (e) {
+    console.log(`Oopsie daisy, this aint working. Here's the message: ${e}`);
+  }
+});
+
+//post requests
+router.post("/create", async (req, res, next) => {
+  try {
+    const { email, password, fullName } = req.body;
+    if (!email || !password || !fullName) {
+      res.status(400).send("missing parameters");
+    } else {
+      const newUser = await User.create({
+        email,
+        passeord,
+        fullName,
+      });
+      res.json(newUser);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
 
 //export
 module.exports = router;
